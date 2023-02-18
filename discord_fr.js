4505199112401d980bf75226758c934a0fa9deae
notevenodd <notevenodd@users.noreply.github.com>
2023-02-18 04:03:26 +0200

    new relex: fake sitelen pona for the toki pona francophone Discord

---
+++ discord_fr.js
@@ -0,0 +1,218 @@
+/* by: jan Polijan
+ *
+ * dictionnary that transforms toki pona names from latin
+ * into sitelen pona look-alike to use on the "toki pona
+ * francophone" Discord group.
+ *
+ * tranformation is such that:
+ * - it is use discord :emojis: from the group if possible,
+ * - or use look-alike unicode characters
+ * - or use some emojis:
+ *   (for colors, genders, "funny" names, or when it's a good match)
+ *
+ *
+ * look alike characters:
+ * i found many and some from firaro: https://firaro.neocities.org/Sitelen%20Pona
+ */
+
+dictionaries.sp_discord_fr = {
+//
+/////////////////////
+// nimi pi wan ala //
+/////////////////////
+'kulupu-nasa'    : ':kulupu_nasa:',
+'toki-pona'      : ':tokipona:',
+'mi_kama'        : ':mi_kama:',
+'mi_tawa'        : ':mi_tawa:',
+'o_lape_pona'    : ':o_lape_pona:',
+'ni_li_pona'     : ':ni_li_pona:',
+'ni_li_ike'      : ':ni_li_ike:',
+'pona_tawa_mi'   : ':pona_tawa_mi:',
+'sina_wawa'      : ':wawa_sina:',
+'wawa_sina'      : ':wawa_sina:',
+//
+////////////////
+// nimi panke //
+////////////////
+'wiwi'               : ':wiwi:',
+'olala'              : ':olala:',
+'panke'              : ':panke:',
+'masalo'             : ':masalo:',
+'sapelipope'         : ':sapelipope:',
+'kalapisituwi'       : '῀õ῀',
+'kalapisituji'       : '῀õ῀', // (=kalapisituwi)
+'kikolo'             : ':kikolo:',
+'pipolo'             : ':face_with_cowboy_hat:',﻿
+'kikulo'             : '(ᶫ.ᶫ)',
+'sikako'             : ':snail:',
+'kokoliko'           : ':rooster:',
+'apelo'              : ':apelo:',
+'kosikosa'           : '≃',
+'antikontitutonelema': ':classical_building:', //:house_abandoned:
+//
+/////////////
+// nimi pu //
+/////////////
+'a'              : ':a_:',
+'akesi'          : '≡Ö≡',
+'ala'            : ':ala:',
+'alasa'          : 'Đ→', // ':bow_and_arrow:',
+'ale'            : '∞',
+'ali'            : '∞', // (= ale)
+'anpa'           : '└─̣┘',
+'ante'           : '><',
+'anu'            : 'Ｙ', // halfwidt&fullformY  // 'Y',
+'awen'           : '𓂜', // _ᐱ_
+'e'              : '⨠', // '>>', //'»' // '>>', 
+'en'             : '+',  // :heavy_plus_sign:
+'esun'           : 'ℒ',
+'ijo'            : ':o:',
+'ike'            : ':ike:',
+'ilo'            : '中',
+'insa'           : '└̇┘',
+'jaki'           : ':jaki:',
+'jan'            : ':jan:',
+'jelo'           : ':yellow_square:',
+'jo'             : 'G̊',
+'kala'           : ':fish:',
+'kalama'         : ':kalama:',
+'kama'           : '𓂻', // '_╱╲̲',
+'kasi'           : ':seedling:', // ܤ
+'ken'            : 'K',
+'kepeken'        : ' ̬n̍͆', // 'ᕄ̍͆ ',   // :mahjong:
+'kili'           : ':apple:',
+'kiwen'          : ':gem:',
+'ko'             : 'ꕤ',
+'kon'            : '∬',
+'kule'           : '🜁', // '△̶'
+'kulupu'         : ':kulupu:',
+'kute'           : ':ear:',
+'la'             : '）', //halfwidth&fullform ) ')',
+'lape'           : '⊸',
+'laso'           : ':blue_square:', // :blue_square: :green_square:
+'lawa'           : 'Ꝋ',   // '𑁌', //Ꝋ
+'len'            : '𐘮', // '╒╤╕'
+'lete'           : '⚹',   // :snowflake:
+'li'             : '>',  //＞
+'lili'           : 'v', // '⌄',
+'linja'          : '～',
+'lipu'           : ':lipu:',
+'loje'           : ':red_square:', // :red_square:
+'lon'            : ':lon:',
+'luka'           : ':back_of_hand:', //'ᕄ',
+'lukin'          : 'ᯣ', // :eye:
+'lupa'           : 'ᑌ',
+'ma'             : '᪠',
+'mama'           : 'O̥',
+'mani'           : '𑀫', //'ᴕ', // :taurus:
+'meli'           : ':female_sign:',
+'mi'             : 'ᑭ',
+'mije'           : ':male_sign:',
+'moku'           : 'ᕄ̆',
+'moli'           : '(ᕯ)', // :skull:
+'monsi'          : '·[',
+'mu'             : ':mu:',
+'mun'            : ':first_quarter_moon_with_face:',
+'musi'           : ':musi:',
+'mute'           : 'ꔖ', // '|||', // | would interfere with Discord
+'nanpa'          : 'ⵌ', // better than ascii # which may trigger stuff on Discord, :hash:
+'nasa'           : ':nasa:',
+'nasin'          : ':nasin:',
+'nena'           : 'ᑎ',
+'ni'             : ':ni:',
+'nimi'           : '▭',
+'noka'           : 'Lᕊ',  //:foot:
+'o'              : ':exclamation:', // 'I̥',
+'olin'           : ':olin:',
+'ona'            : 'ᓄ',
+'open'           : '╘╛',
+'pakala'         : ':pakala:',
+'pali'           : ':pali:',
+'palisa'         : '꧰',
+'pan'            : '𐠃',
+'pana'           : 'ˈ̬n̍´', // '\' ̬n̍´', // 'ᕄ⃛',
+'pi'             : 'ᒪ',
+'pilin'          : ':pilin:',
+'pimeja'         : '⨻', // ':black_large_square:', // bad on dark discord background
+'pini'           : '𝙸', // <--- mathfont monspace, 'エ',
+'pipi'           : '⽺',
+'poka'           : '⊔·',
+'poki'           : '⊔',
+'pona'           : ':pona:',
+'pu'             : ':pu:',
+'sama'           : ':sama:',
+'seli'           : '\\!/', //'\\Ɩ̣/',
+'selo'           : '╓╖', // ℿ
+'seme'           : ':seme:',
+'sewi'           : 'ﺳ‎ı',
+'sijelo'         : '𐊿', // '┌┬┐',
+'sike'           : '◎',
+'sin'            : ':sin:',
+'sina'           : 'ᑲ',
+'sinpin'         : ']·',
+'sitelen'        : ':sitelen:',
+'sona'           : ':sona:',
+'soweli'         : 'ııïᕈ',
+'suli'           : 'Ｖ', //<--halfwidth&fullform   '╲╱', // '⋁', '╲╱'
+'suno'           : ':high_brightness:', // '𐄢',  // :high_brightness: , :sunny:
+'supa'           : '┬┬',
+'suwi'           : ':suwi:',
+'tan'            : ':tan:',
+'taso'           : ':taso:',
+'tawa'           : '𓂽', //       '╱̲╲_',   // 𓂽
+'telo'           : '≈',
+'tenpo'          : ':clock3:',
+'toki'           : ':toki:',
+'tomo'           : '⌂',
+'tu'             : '꠱', // '||' would interfere with discord
+'unpa'           : 'Ꮱ', // '𓂸', // ᶜᵤᵓ
+'uta'            : '𓂑', // '𐃬',
+'utala'          : ':utala:',
+'walo'           : 'ꕖ' , // ':white_large_square:', // bad on light discord background
+'wan'            : '↿',
+'waso'           : '"ᔨ',
+'wawa'           : ':wawa:',
+'weka'           : ':anger:',
+'wile'           : ':wile:',
+//
+//////////////////
+// nimi ku suli //
+//////////////////
+'namako'         : '⊹', // '╶╎╴'
+'kin'            : ':kin:',
+'oko'            : ':eye:', // ⏿   ⩹
+'kipisi'         : '％', //faltwidth&fullform %
+'leko'           : '⧈', // :white_square_button:
+'monsuta'        : ':part_alternation_mark:',
+'tonsi'          : ':transgender_symbol:',
+'jasima'         : 'ꬸ', // '⍭'
+'kijetesantakalu': ':raccoon:',
+'soko'           : ':mushroom:',
+'meso'           : '|·|',
+'epiku'          : ':arrow_up:', // ':superhero:', ///???
+'kokosila'       : '`Ø̍´',
+'lanpan'         : 'ሮ', // 'ᝰ.', // bof bof!
+'n'              : 'n̍',
+'misikeke'       : ':pill:',
+'ku'             : ':ku:',
+//
+
+
+
+
+///////////////
+// nimi ante //
+///////////////
+'apeja'          : '˙⟑˙',
+'pake'           : 'ㄒ',
+'powe'           : '⨱',
+'wawajete'       : '¤',
+'yupekosi'       : 'y',
+'majuna'         : '╶╷╴', // :lotus: --
+'kapesi'         : ':brown_square:', // '⨹',
+'unu'            : ':purple_square:',
+'nalanja'        : ':orange_square:',
+'mulapisu'       : ':pizza:',
+'isipin'         : ':thinking_face:',
+'linluwi'        : ':spider_web:',
+};
