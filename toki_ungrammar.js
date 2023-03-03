e6bbe703b8d077ebf1d9a48c8ec548ed7aeec615
notevenodd <notevenodd@users.noreply.github.com>
2023-03-03 14:25:59 +0200

    micro dictionnary "ungrammar" which juste removes several particles

---
+++ toki_ungrammar.js
@@ -0,0 +1,18 @@
+/* by: jan Polijan
+ *
+ *   -=[ This tiny dictionnary removes important particles from toki pona ]=-
+ * (Often, people learning the words but who can't really speak toki pona, make
+ * sentences with just chains of toki pona words. Sometimes it's comprehensible.
+ * But... how  far is it comprehensible? That's the point of this "dictionnary".
+ */
+
+dictionaries.ungrammar = {
+'pi'   : '',
+'li'   : '',
+'e'    : '',
+'la'   : '',
+//'ni' : '',
+//'o'  : '',
+//'en' : '',
+//'ona': '',
+};
